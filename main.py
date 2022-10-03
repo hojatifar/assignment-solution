7b7cc1142f9e4d1a889f4343711841158ca3a72b
shaahin <68428907+hojatifar@users.noreply.github.com>
2022-10-03 15:38:25 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import output_txt
 # start point of tool
 if __name__ == '__main__':
     # get path of log files from user input
-    path = input('Enter path of log files:\n')
+    path = input('Enter path of log files (Directory of log files):\n')
     # check the user input for correct path of files
     while not os.path.isdir(path):
-        path = input('Path is not correct! Please enter path (Directory of files) of log files:\n')
+        path = input('Path is not correct! Please enter path (Directory of log files):\n')
 
     input_log_files.get_logs(path)  # TODO : should implement in thread for future and real scenario
     output_txt.write_file()
